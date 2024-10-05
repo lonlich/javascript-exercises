833722f017302e9521a496e5ac449e5fb1ec1ace
lonely <orenkin@yandex.ru>
2024-10-05 16:27:19 +0300

    Do exercise #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,21 +3,22 @@ const reverseString = function(input) {
 //2. текущая итерация лупа = индексу новой строки
 //3. индекс инпут строки = начиная с последнего элемента, уменьшая на -1 с каждой итерацией пока индекс не станет = 0
 
-let inputIndex = input.length - 1;
-let resultIndex = 0;
-let result = '';
+//loop implementation
+// let inputIndex = input.length - 1;
+// let resultIndex = 0;
+// let result = '';
 
-while (inputIndex >= 0) {
+// while (inputIndex >= 0) {
 
-    result += input[inputIndex];
+//     result += input[inputIndex];
 
-    inputIndex--;
-    resultIndex++;
-}
-    return result;
+//     inputIndex--;
+//     resultIndex++;
+// }
+//     return result;
+
+    return input.split('').reverse().join('');
 };
-
-reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
