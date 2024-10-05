6655a57c8ed7309de06a9b80ab16a6c9cc75be0e
lonely <orenkin@yandex.ru>
2024-10-05 15:59:30 +0300

    Clone jest repo and do exercises #1 #2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(input) {
+//1. цикл с количеством лупов = длине исходной строки
+//2. текущая итерация лупа = индексу новой строки
+//3. индекс инпут строки = начиная с последнего элемента, уменьшая на -1 с каждой итерацией пока индекс не станет = 0
 
+let inputIndex = input.length - 1;
+let resultIndex = 0;
+let result = '';
+
+while (inputIndex >= 0) {
+
+    result += input[inputIndex];
+
+    inputIndex--;
+    resultIndex++;
+}
+    return result;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
