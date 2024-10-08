b84aea20992cec447e86179ac55da567da2e025f
lonely <orenkin@yandex.ru>
2024-10-08 15:08:57 +0300

    Do exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    
+    if (
+        !(Number.isInteger(firstNum) && Number.isInteger(secondNum) && 
+        firstNum > 0 && 
+        secondNum > 0)
+    ) {
+        return `ERROR`;
+    }
 
+    //меняем цифры местами, если первая больше второй
+    if (firstNum > secondNum) {
+        [firstNum, secondNum] = [secondNum, firstNum]
+    }
+
+    let result = null;
+    for (let i = firstNum; i <= secondNum; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
