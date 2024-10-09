c22ac8bea45fafd73c3f7d1b5cc9420c9ddfdcf9
lonely <orenkin@yandex.ru>
2024-10-09 23:35:07 +0300

    Do exercise #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempF) {
+  const tempC = (tempF - 32) * (5 / 9);
+  return +tempC.toFixed(1)
+}; 
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const tempF = (tempC * (9 / 5) + 32);
+  return +tempF.toFixed(1)
 };
 
 // Do not edit below this line
