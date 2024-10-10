22febd926a425940df3d98f884bc479b343bc9c8
lonlich <30961049+lonlich@users.noreply.github.com>
2024-10-10 19:46:28 +0300

    Do exercise #8 - 1 & 2 functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,9 +1,11 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let total = null; 
+  nums.forEach(num => total += num);
+  return total; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
 const sum = function() {
