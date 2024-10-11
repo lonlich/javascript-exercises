cdf3d9cdd6d1d26b1aae1a4163700e640a4956b9
lonlich <30961049+lonlich@users.noreply.github.com>
2024-10-11 19:13:28 +0300

    Do exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    arr.shift(); //remove 0 from the start of the sequence
+    return arr[num - 1];
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
