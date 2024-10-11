2148f0edb61395020104c6ff7ed7287ba302b2e3
lonlich <30961049+lonlich@users.noreply.github.com>
2024-10-11 19:30:57 +0300

    Do exercise #11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,17 @@
 const fibonacci = function(num) {
+    
+    //do not accept negatives
+    if (num < 0) {
+        return 'OOPS'
+    }
+    
+    //build fib sequence
     const arr = [0, 1];
     for (let i = 2; i <= num; i++) {
         arr[i] = arr[i - 2] + arr[i - 1];
     }
-    arr.shift(); //remove 0 from the start of the sequence
-    return arr[num - 1];
+
+    return arr[num];
 };
 
 fibonacci(4);
