124b4a2519e5924c049782a96ff56fbefb04ff3c
lonely <orenkin@yandex.ru>
2024-10-11 14:07:01 +0300

    Do exercise #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,21 +8,30 @@ const subtract = function(num1, num2) {
   return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0; 
+  array.forEach(item => total += item);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1; 
+  array.forEach(item => total *= item);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }  
+    return num * factorial(num - 1);
 };
+
+factorial(5);
 
 // Do not edit below this line
 module.exports = {
