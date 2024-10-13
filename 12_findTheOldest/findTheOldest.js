722a30af0ac2020a4924d15c453e769a448f333d
lonely <orenkin@yandex.ru>
2024-10-13 16:11:03 +0300

    Start DOM lesson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,35 @@
 const findTheOldest = function(arr) {
-
     let now = new Date();
     const currentYear = now.getFullYear();
+    
+    return arr.reduce((oldest, current) => {
 
-    const allAges = arr.map(person => {
-        if (!person.yearOfDeath) {
-            person.yearOfDeath = currentYear;
-        }
-        return person.yearOfDeath - person.yearOfBirth;
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const currentDeathYear = current.yearOfDeath || currentYear;
+        
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        const currentAge = currentDeathYear - current.yearOfBirth;
+        return currentAge > oldestAge ? current : oldest;
     })
+};
+    //Array.map approach (more readable but have to create intermediate entities :( )
 
-    const maxAge = Math.max(...allAges);
-    const maxIndex = allAges.indexOf(maxAge);
+    // let now = new Date();
+    // const currentYear = now.getFullYear();
 
-    return arr[maxIndex];
+    // const allAges = arr.map(person => {
+    //     if (!person.yearOfDeath) {
+    //         person.yearOfDeath = currentYear;
+    //     }
+    //     return person.yearOfDeath - person.yearOfBirth;
+    // })
+
+    // const maxAge = Math.max(...allAges);
+    // const maxIndex = allAges.indexOf(maxAge);
+
+    // return arr[maxIndex];
+
+    //Loop approach
 
 //     let age;
 //     let prevAge = 0;
@@ -32,27 +48,27 @@ const findTheOldest = function(arr) {
 //     prevAge = age;
 // }
 //     return arr[j];
-};
+//};
 
-// const people = [
-//     {
-//       name: "Carly",
-//       yearOfBirth: 1942,
-//       yearOfDeath: 1970,
-//     },
-//     {
-//       name: "Ray",
-//       yearOfBirth: 1962,
-//       yearOfDeath: 2011,
-//     },
-//     {
-//       name: "Jane",
-//       yearOfBirth: 1912,
-//       yearOfDeath: 1941,
-//     },
-//   ]
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-// findTheOldest(people);
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
